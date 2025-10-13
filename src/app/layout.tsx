f1b2f22a0a6ae38ab25c3ffdf970fb71c46d5e94
jao347 <jkris347@gmail.com>
2025-10-13 18:09:40 +0800

    clarity fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
 
             <Script
               id="microsoft-clarity"
-              strategy="afterInteractive"
+              strategy="lazyOnload"
               dangerouslySetInnerHTML={{
                 __html: `
                 (function(c,l,a,r,i,t,y){
